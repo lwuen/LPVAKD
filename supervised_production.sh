75c0ec98951130c7a001b9d43823e69da7476d30
lwuen <48399031+lwuen@users.noreply.github.com>
2024-07-12 10:22:53 +0800

    Delete supervised_production.sh

--- supervised_production.sh
+++
@@ -1,7 +0,0 @@
-python ../src/train_teacher_gnn.py --datasets=cora --encoder=gcn --runs=10 --transductive=production
-python ../src/train_teacher_gnn.py --datasets=citeseer --encoder=gcn --runs=10 --transductive=production
-python ../src/train_teacher_gnn.py --datasets=pubmed --encode=gcn --runs=10 --transductive=production
-python ../src/train_teacher_gnn.py --datasets=coauthor-cs --encoder=gcn --runs=10 --transductive=production
-python ../src/train_teacher_gnn.py --datasets=coauthor-physics --encoder=gcn --runs=10 --transductive=production --hidden_channels=256
-python ../src/train_teacher_gnn.py --datasets=amazon-computers --encoder=gcn --lr=0.001 --runs=10 --transductive=production
-python ../src/train_teacher_gnn.py --datasets=amazon-photos --encoder=gcn --lr=0.001 --runs=10 --transductive=production
